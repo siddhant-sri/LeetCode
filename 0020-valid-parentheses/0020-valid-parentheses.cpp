9bea3de52421f218157f6fa50b95dab742eca2a3
siddhant-sri <63922624+siddhant-sri@users.noreply.github.com>
2024-01-30 00:23:14 +0530

    Time: 3 ms (44.50%), Space: 7.4 MB (11.60%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto c : s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(st.empty())  return false;
+                else{
+                    if((c==')' && st.top()=='(') || 
+                        (c=='}' && st.top()=='{') || 
+                        (c==']' && st.top()=='[')) {
+                        st.pop();
+                    } else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return (st.empty());
+    }
+};
