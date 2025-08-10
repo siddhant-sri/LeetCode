cbe7c103d32cf5d2f4d5914d1e92ff732c054165
Siddhant Srivastava <63922624+siddhant-sri@users.noreply.github.com>
2025-08-10 18:36:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.88%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int minMaxDifference(int num) {
-        // Used with STL lib, refer first submission for detail
-        string str1 = to_string(num);
-        string str2 = str1;
-        
-        // Max Number in string
-        int idx = str1.find_first_not_of('9');
-        if(idx != string::npos){
-            char ch = str1[idx];
-            replace(begin(str1), end(str1), ch , '9');
+        string str_num = to_string(num);
+        string maxStr = "";
+        string minStr = "";
+        char ch;
+
+        // Max number in string
+        for (int i=0; i<str_num.length(); i++){
+            if(str_num[i] != '9'){
+                ch = str_num[i]; 
+                break;
+            }  
+        }
+        for(int i=0; i<str_num.length(); i++){
+            if(str_num[i] == ch){
+                maxStr += '9';
+            } else {
+                maxStr += str_num[i];               
+            }
         }
 
-        // Min Number in string
-        char ch = str2[0];
-        replace(begin(str2), end(str2), ch, '0');
+        // Min number in string
+        ch = str_num[0];
+        for(int i=0; i<str_num.length(); i++){
+            if(str_num[i] == ch){
+                minStr += '0';
+            } else {
+                minStr += str_num[i];               
+            }
+        }
 
-        return stoi(str1) - stoi(str2);
+        return stoi(maxStr) - stoi(minStr);
     }
 };
