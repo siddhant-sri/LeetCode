53087d5dad0ed222d1c1b5f35f450198d6b1dc4f
Siddhant Srivastava <63922624+siddhant-sri@users.noreply.github.com>
2025-08-31 20:00:17 +0530

    Time: 0 ms (100%), Space: 33.8 MB (95.09%) - LeetHub

--- 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
+++ 3973-flip-square-submatrix-vertically/3973-flip-square-submatrix-vertically.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        // Make a copy of the original grid
-        vector<vector<int>> res = grid;
+        int er = x+k-1;
+        int ec = y+k-1;
 
-        int er = x + k -1;
-        int ec = y + k -1;
-
-        int row = er;
-        
-        for(int i=x; i<=er; i++){
+        while(x < er){
             for(int j=y; j<=ec; j++){
-                res[row][j] = grid[i][j];
+                swap(grid[x][j], grid[er][j]);
             }
-            row--;
+            x++;
+            er--;
         }
-        return res;
+        return grid;
     }
 };
