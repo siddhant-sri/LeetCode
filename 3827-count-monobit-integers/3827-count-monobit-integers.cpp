f297e08ec497f4787aa809923a8e55e933c9d4df
Siddhant Srivastava <63922624+siddhant-sri@users.noreply.github.com>
2026-02-03 21:44:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (26.6%) - LeetHub

--- 3827-count-monobit-integers/3827-count-monobit-integers.cpp
+++ 3827-count-monobit-integers/3827-count-monobit-integers.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int countMonobit(int n) {
-      int count = 0;
+      int count = 1;
+      int num = 1;
 
-        for (int i = 0; i <= n; i++) {
-            if (i == 0 || ((i & (i + 1)) == 0)) {
-                count++;
-            }
-        }
+      while(num <= n){
+        count++;
+        num = (num << 1) | 1; 
+      }
+
         return count;  
     }
 };
